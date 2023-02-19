71e840eaee1285cdd30c4f03ef1db450e78859f5
Moses Antonio <moses.antonio@stud.h-da.de>
2023-02-19 19:56:25 +0100

    update Translator.py

--- Translator.py
+++ Translator.py
@@ -3,15 +3,20 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+
+#const
 SOURCE_FILE = "input.txt"
+PATH = r'C:\Users\moses\AppData\Roaming\Mozilla\Firefox\Profiles\mm37urk0.default-release'
+WEB_URL = "https://www.deepl.com/translator#auto/en/"
+
+#var
 text = ""
 loop_num = 1
 
 def get_options():
-    path = r'C:\Users\moses\AppData\Roaming\Mozilla\Firefox\Profiles\mm37urk0.default-release'
     options = Options()
     options.add_argument("-profile")
-    options.add_argument(path)
+    options.add_argument(PATH)
     return options
 
 def print_log(num):
@@ -31,11 +36,10 @@ while True:
                 text = text + line.replace("\n", " ").replace("/", " oder ").replace(".",".%0A%0A")
 
     if text != prev_string:
-        driver.get("https://www.deepl.com/translator#auto/en/" + text)
+        driver.get(WEB_URL + text)
         print_log(loop_num)
         loop_num += 1
 
     time.sleep(1)
-
 
 
